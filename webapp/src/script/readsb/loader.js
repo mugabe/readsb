91ab9893b77398f2edbf963f26d68a74ff08249a
Matthias Wirth <matthias.wirth@gmail.com>
2019-12-28 14:10:16 +0100

    minimized net only branch

--- webapp/src/script/readsb/loader.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var READSB;
-(function (READSB) {
-    document.addEventListener("DOMContentLoaded", READSB.Database.Init.bind(READSB.Database));
-})(READSB || (READSB = {}));
-//# sourceMappingURL=loader.js.map
